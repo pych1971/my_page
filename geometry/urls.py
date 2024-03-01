a841ca0323fa24a4609f104e30ae375fc365f5eb
pych1971 <pych1971@gmail.com>
2024-03-01 17:37:22 +0500

    2024-03-01-17-37

--- geometry/urls.py
+++ geometry/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rectangle/<int:width>/<int:height>/', views.get_rectangle_area),
-    path('square/<int:width>/', views.get_square_area),
-    path('circle/<int:radius>/', views.get_circle_area),
+    path('rectangle/<int:width>/<int:height>', views.get_rectangle_area),
+    path('get_rectangle_area/<int:width>/<int:height>', views.get_rectangle_area_redirect),
+    path('square/<int:width>', views.get_square_area),
+    path('get_square_area/<int:width>', views.get_square_area_redirect),
+    path('circle/<int:radius>', views.get_circle_area),
+    path('get_circle_area/<int:radius>', views.get_circle_area_redirect),
 ]
--- geometry/views.py
+++ geometry/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 # Create your views here.
-# def monday(request):
-#     return HttpResponse("Дела на понедельник")
-#
-#
-# def tuesday(request):
-#     return HttpResponse("Дела на вторник")
 
 def get_rectangle_area(request, width, height):
     return HttpResponse(f"Площадь прямоугольника размером {width}x{height} равна {width * height}")
+
+
+def get_rectangle_area_redirect(request, width, height):
+    return HttpResponseRedirect(f"/calculate_geometry/rectangle/{width}/{height}")
 
 
 def get_square_area(request, width):
     return HttpResponse(f"Площадь квадрата размером {width}x{width} равна {width ** 2}")
 
 
+def get_square_area_redirect(request, width):
+    return HttpResponseRedirect(f"/calculate_geometry/square/{width}")
+
+
 def get_circle_area(request, radius):
     return HttpResponse(f"Площадь круга с радиусом {radius} равна {3.14 * radius ** 2}")
+
+
+def get_circle_area_redirect(request, radius):
+    return HttpResponseRedirect(f"/calculate_geometry/circle/{radius}")
