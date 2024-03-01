a7ee1756978f537dff6a00a180662ef0dc894411
pych1971 <pych1971@gmail.com>
2024-03-01 11:52:07 +0500

    2024-03-01-11-52

--- geometry/views.py
+++ geometry/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+
 
 # Create your views here.
+# def monday(request):
+#     return HttpResponse("Дела на понедельник")
+#
+#
+# def tuesday(request):
+#     return HttpResponse("Дела на вторник")
+
+def get_rectangle_area(request, width, height):
+    return HttpResponse(f"Площадь прямоугольника размером {width}x{height} равна {width * height}")
+
+
+def get_square_area(request, width):
+    return HttpResponse(f"Площадь квадрата размером {width}x{width} равна {width ** 2}")
+
+
+def get_circle_area(request, radius):
+    return HttpResponse(f"Площадь круга с радиусом {radius} равна {3.14 * radius ** 2}")
--- my_page/urls.py
+++ my_page/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('horoscope/', include('horoscope.urls')),
     path('todo_week/', include('week_days.urls')),
+    path('calculate_geometry/', include('geometry.urls')),
 ]
