e53ee70c59b2cb214843dbae7cd47562fc465bc7
pych1971 <pych1971@gmail.com>
2024-03-02 16:00:14 +0500

    2024-03-02-16-00

--- horoscope/views.py
+++ horoscope/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -32,4 +33,5 @@ def get_info_about_sign_zodiac_by_number(request, sign_zodiac: int):
     if sign_zodiac > len(zodiacs):
         return HttpResponseNotFound(f'Неправильный порядковый номер знака зодиака - {sign_zodiac}')
     name_zodiac = zodiacs[sign_zodiac - 1]
-    return HttpResponseRedirect(f'/horoscope/{name_zodiac}')
+    redirect_url = reverse('horoscope-name', args=[name_zodiac])
+    return HttpResponseRedirect(redirect_url)
