69d60edc2728b56dd302fb077c82c53dad875f94
pych1971 <pych1971@gmail.com>
2024-03-03 14:03:57 +0500

    2024-03-03-14-03

--- horoscope/tests.py
+++ horoscope/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from horoscope.views import zodiac_dict
 
 
 # Create your tests here.
@@ -14,6 +15,12 @@ class TestHoroscope(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
                       response.content.decode())
+
+    def test_signs(self):
+        for sign in zodiac_dict:
+            response = self.client.get(f'/horoscope/{sign}')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(zodiac_dict[sign], response.content.decode())
 
     def test_libra_redirect(self):
         response = self.client.get('/horoscope/7')
